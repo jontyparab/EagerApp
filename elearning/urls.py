cceb3be2a6dca59642c1a1240f7c2c04cf39992a
Mandar Parab <jontyparab@gmail.com>
2021-08-09 20:03:15 +0530

    test

--- elearning/urls.py
+++ elearning/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from settings import STATIC_ROOT
+from .settings import STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
