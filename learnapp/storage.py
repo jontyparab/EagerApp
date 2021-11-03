f2548fc6523aaa53b38eacaa4ef294282443da09
Mandar Parab <jontyparab@gmail.com>
2021-11-03 20:18:57 +0530

    storage uses query params

--- learnapp/storage.py
+++ learnapp/storage.py
@@ -40,7 +40,8 @@ class FileStorageView(APIView):
 
     def get(self, request):
         try:
-            files_type = request.GET.get('type')
+            files_type = request.query_params.get('type')
+
             if files_type == 'backgrounds':
                 url_arr = self.static_files_getter()
                 return Response({
